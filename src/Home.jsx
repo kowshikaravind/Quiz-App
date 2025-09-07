1051016ff5adb29b8f84120aa0fed5f3a0ec6f2c
kowshikaravind4@gmail.com <kowshikaravind4@gmail.com>
2025-09-07 17:18:58 +0530

    Hamberger menu

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
+import Hamberger from 'hamburger-react';
 
 function Home() {
   const location = useLocation();
@@ -8,6 +9,7 @@ function Home() {
   const storedDetails = JSON.parse(localStorage.getItem("userDetails")) || {};
   const [user, setUser] = React.useState(stateUser || storedDetails.user || "");
   const [email, setEmail] = React.useState(stateEmail || storedDetails.email || "");
+  const [open , setOpen] = React.useState(false);
 
   const navigate = useNavigate();
   const loginbutton = user ? 'Logout' : 'Login';
@@ -38,20 +40,26 @@ function Home() {
   return (
     <div>
       <div className='header'>
-        <div className='header-top'>
-          <p className='welcome-user'>Welcome {user} !</p>
-          <button className='head-btn' onClick={() => navigate('/DailyChallenge')}>Daily Challenge</button>
+        <div className='hamberger-menu'>
+          <Hamberger toggled={open} toggle={setOpen} />
         </div>
-        <div className='header-bottom'>
-          <button className='head-btn' onClick={() =>navigate('/Aboutus')}>About us</button>
-          <button className='head-btn' onClick={handleClick}>{loginbutton}</button>
-          <button className='head-btn' onClick={handleSetting}>Settings</button>
-        </div>
+        {open && <div className='hamberger-menu-content'>
+          <div className='header-top'>
+            <p className='welcome-user'>Welcome {user} !</p>
+            <button className='head-btn' onClick={() => navigate('/DailyChallenge')}>Daily Challenge</button>
+          </div>
+          <div className='header-bottom'>
+            <button className='head-btn' onClick={() =>navigate('/Aboutus')}>About us</button>
+            <button className='head-btn' onClick={handleClick}>{loginbutton}</button>
+            <button className='head-btn' onClick={handleSetting}>Settings</button>
+          </div>
+          </div>}
       </div>
       <div className='main-content'>
         <h1>The QuizArena Welcomes You</h1>
         <button className='start-btn' onClick={handleStart}>Start Quiz</button>
       </div>
+      
     </div>
   );
 }
